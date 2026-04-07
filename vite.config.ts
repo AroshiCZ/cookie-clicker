6708721678ef0dc53b5356db10d35d0087239e59
AroshiCZ <bohmda22@sps-prosek.cz>
2026-04-07 09:22:31 +0200

    added firebase

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cc/',
   build: {
     outDir: '../cc',   // 👈 your target folder
     emptyOutDir: true
